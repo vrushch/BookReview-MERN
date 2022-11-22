fdfd69a9bb381b6df3b818a8638107d153aa37aa
Vrushal Chaudhari <vrushalcs@gmail.com>
2022-11-22 15:53:25 -0800

    adding cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 5001;
 const bookRouter = require("./routes/books");
 const reviewRouter = require("./routes/reviews")
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/books",bookRouter);
 app.use("/api/reviews", reviewRouter);
+app.use(cors());
 
 app.get("/",function(req,res){
     res.send("booksapi")
