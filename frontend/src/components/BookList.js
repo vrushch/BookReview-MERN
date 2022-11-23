d4a627ea7569aa639da567a65c7a087357f574d7
Vrushal Chaudhari <vrushalcs@gmail.com>
2022-11-22 16:54:56 -0800

    adding routing

--- frontend/src/components/BookList.js
+++ frontend/src/components/BookList.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 const BookList = (props) => {
+    const [books, setBooks] = useState([]);
+
+	const getBookRequest = async () => {
+    const url = "https://project-3-backend-fevm.onrender.com/api/books";
+		const response = await fetch(url);
+		const responseJson = await response.json();
+		if (responseJson) {
+			setBooks(responseJson);
+		}
+	};
+    
+    
+	useEffect(() => {
+		getBookRequest();
+	}, []);
+
+
 	return (
 		<>
-			{props.books.map((book, index) => (
+			{books.map((book, index) => (
 				<div className='image-container d-flex justify-content-start m-3' key={index}>
 					<img src={book.volumeInfo.imageLinks.thumbnail} alt='book'></img>
 				</div>
