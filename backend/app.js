ad8cac145ffc63f394d1657a0ae2cda763feeb40
Vrushal Chaudhari <vrushalcs@gmail.com>
2022-11-23 15:21:28 -0800

    adding cors function

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,39 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const port = 5001;
 const bookRouter = require("./routes/books");
-const reviewRouter = require("./routes/reviews")
-const {connectToDB} = require('./db');
+const reviewRouter = require("./routes/reviews");
+const { connectToDB } = require("./db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api/books",bookRouter);
-app.use("/api/reviews", reviewRouter);
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT,DELETE, PATCH"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
-app.get("/",function(req,res){
-    res.send("booksapi")
-    
+app.use("/api/books", bookRouter);
+app.use("/api/reviews", reviewRouter);
+
+app.get("/", function (req, res) {
+  res.send("booksapi");
 });
 
 app.use((req, res, next) => {
-    res.status(404).send("Not found");
-})
+  res.status(404).send("Not found");
+});
 
-app.listen(port, () =>{
-    console.log(`listening on ${port}`);
-    connectToDB();
-})
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+  connectToDB();
+});
