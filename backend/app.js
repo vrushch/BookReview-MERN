56295da74c6e0ba1708a8ef129cc351ec5aa5583
Vrushal Chaudhari <vrushalcs@gmail.com>
2022-11-26 12:56:40 -0800

    adding oauth

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 5001;
 const bookRouter = require("./routes/books");
 const reviewRouter = require("./routes/reviews");
+const userRouter = require("./routes/users");
 const { connectToDB } = require("./db");
+const { auth } = require("express-openid-connect");
+require("dotenv").config();
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.secret,
+  baseURL: process.env.baseURL,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL,
+};
+app.use(auth(config));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,9 +36,11 @@ app.use(function (req, res, next) {
 
 app.use("/api/books", bookRouter);
 app.use("/api/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.get("/", function (req, res) {
-  res.send("booksapi");
+  //res.send("booksapi");
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
 
 app.use((req, res, next) => {
