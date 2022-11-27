2b7b04f21ac1d9b5d9b7d9abe6fba8fb3fa7a0f4
Vrushal Chaudhari <vrushalcs@gmail.com>
2022-11-26 23:02:21 -0800

    syncing user with database

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,21 +2,31 @@ const express = require("express");
 const router = express.Router();
 const { requiresAuth } = require("express-openid-connect");
 
+const {
+  addToDB,
+  readAll,
+  readOne,
+  deleteOne,
+  updateOne,
+  readAllWithFilter,
+} = require("../db");
+const { ObjectId } = require("mongodb");
+const collectionName = "users";
+
 router.get("/profile", requiresAuth(), async (req, res) => {
-  //res.send(JSON.stringify(req.oidc.user));
-  const auth0Id = req.oidc;
-  console.log(auth0Id);
-  //const user = await prisma.user.findUnique({
-  //  where: {
-  //    auth0Id,
-  //  },
-  //});
-  res.send(JSON.stringify(req.oidc.user));
-  //if (user) {
-  //  res.json(user);
-  //} else {
-  //  res.status(404).send("User not found");
-  //}
+  const user_email = req.oidc.email;
+
+  try {
+    const data = await readOne({ _id: ObjectId(user_email) }, collectionName);
+    if (data) {
+      res.json(data);
+    } else {
+      res.status(404).send("User Not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
