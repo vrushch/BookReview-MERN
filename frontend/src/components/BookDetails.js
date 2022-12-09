2c583d8ab622fef12642ea30cfe56a154f47c938
Karishma Vadsaria <vadsariakarishma@gmail.com>
2022-12-09 15:21:41 -0800

    Structure the book details page

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useAuthToken } from "../auth/AuthTokenContext";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 export default function BookDetails() {
   const [bookDetails, setBookDetails] = useState(0);
@@ -137,87 +139,228 @@ export default function BookDetails() {
     <>
       {bookDetails && reviewsDetails && viewAllReviews && (
         <>
-          <h2>The details of book</h2>
-          <h3>Title</h3>
-          <div>{bookDetails.volumeInfo.title}</div>
-          <div class="col-md-12 bg-light text-right">
-            <button
-              type="button"
-              class="btn btn-success"
-              onClick={() => {
-                window.open(
-                  `https://vpl.bibliocommons.com/v2/search?query=${bookDetails.volumeInfo.title}`
-                );
-              }}
-            >
-              Borrow book
-            </button>
-          </div>
-          <h3>Subtitle</h3>
-          <div>{bookDetails.volumeInfo.subtitle}</div>
-          <h3>Description</h3>
-          <div>{bookDetails.volumeInfo.description}</div>
-          <h3>Reviews</h3>
-          {!editReview && (
-            <button
-              onClick={() => {
-                setAddReview(true);
-                setEditReview(true);
-              }}
-            >
-              Add New Review{" "}
-            </button>
-          )}
-          {!editReview ? (
-            reviewsDetails.map((review, index) => {
-              return (
-                <div key={index}>
-                  {review.review_text}
-                  <button onClick={() => deleteReview(review._id)}>
-                    Delete
-                  </button>
-                  <button
-                    onClick={() => {
-                      setEditReview(true);
-                      setEditIndex(index);
-                    }}
-                  >
-                    Update
-                  </button>
+          <div className="container">
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-title">{bookDetails.volumeInfo.title}</h3>
+                <h6 className="card-subtitle">
+                  {bookDetails.volumeInfo.subtitle}
+                </h6>
+                <div className="row">
+                  <div className="col-lg-5 col-md-5 col-sm-2">
+                    <div className="white-box text-center">
+                      <img
+                        src={bookDetails.volumeInfo.imageLinks.thumbnail}
+                        className="img-responsive"
+                        alt="book"
+                      ></img>
+                    </div>
+                  </div>
+                  <div className="col-lg-7 col-md-7 col-sm-6">
+                    <h4 className="box-title mt-5">Book description</h4>
+                    <p>{bookDetails.volumeInfo.description}</p>
+                  </div>
+                  <div className="col-lg-12 col-md-12 col-sm-12">
+                    <h3 className="box-title mt-5">Book Reviews</h3>
+                    <div>
+                      {!editReview && (
+                        <button
+                          className="btn btn-info add-new"
+                          onClick={() => {
+                            setAddReview(true);
+                            setEditReview(true);
+                          }}
+                        >
+                          <i className="fa fa-plus"></i>
+                          Add New Review{" "}
+                        </button>
+                      )}
+                    </div>
+                    <div className="table-responsive">
+                      <table className="table table-striped table-product">
+                        <tbody>
+                          <tr>
+                            <td>
+                              {!editReview ? (
+                                reviewsDetails.map((review, index) => {
+                                  return (
+                                    <div key={index}>
+                                      {review.review_text}
+                                      <span className="edit_delete">
+                                        <EditIcon
+                                          className="edit_icon"
+                                          onClick={(handleClick) => {
+                                            setEditReview(true);
+                                            setEditIndex(index);
+                                          }}
+                                        />
+                                        <DeleteIcon
+                                          className="delete_icon"
+                                          onClick={() =>
+                                            deleteReview(review._id)
+                                          }
+                                        />
+                                      </span>
+                                    </div>
+                                  );
+                                })
+                              ) : (
+                                <div>
+                                  <input
+                                    placeholder=" Enter Review"
+                                    value={editReviewText}
+                                    onChange={(e) =>
+                                      setEditReviewText(e.target.value)
+                                    }
+                                  />
+                                  {addReview ? (
+                                    <button
+                                      className="btn btn-success"
+                                      onClick={() => {
+                                        createReview(editReviewText);
+                                      }}
+                                    >
+                                      Create{" "}
+                                    </button>
+                                  ) : (
+                                    <button
+                                      className="btn btn-success"
+                                      onClick={() =>
+                                        saveReview(
+                                          reviewsDetails[editIndex]._id,
+                                          editReviewText
+                                        )
+                                      }
+                                    >
+                                      Save
+                                    </button>
+                                  )}
+                                </div>
+                              )}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <h4 className="box-title mt-5">
+                                Overall Reviews
+                              </h4>
+                              {viewAllReviews.map((review, index) => {
+                                return (
+                                  <table className="table table-striped table-product">
+                                    <tbody>
+                                      <tr>
+                                        <td>
+                                          <div key={index}>
+                                            {review.review_text}
+                                          </div>
+                                        </td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                );
+                              })}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
                 </div>
-              );
-            })
-          ) : (
-            <div>
-              <input
-                placeholder="Enter New Review"
-                value={editReviewText}
-                onChange={(e) => setEditReviewText(e.target.value)}
-              />
-              {addReview ? (
-                <button
-                  onClick={() => {
-                    createReview(editReviewText);
-                  }}
-                >
-                  Create{" "}
-                </button>
-              ) : (
-                <button
-                  onClick={() =>
-                    saveReview(reviewsDetails[editIndex]._id, editReviewText)
-                  }
-                >
-                  Save
-                </button>
-              )}
+              </div>
             </div>
-          )}
-          {viewAllReviews.map((review, index) => {
-            return <div key={index}>{review.review_text}</div>;
-          })}
+          </div>
         </>
       )}
     </>
   );
+
+  // return (
+  //   <>
+  //     {bookDetails && reviewsDetails && viewAllReviews && (
+  //       <>
+  //         <h2>The details of book</h2>
+  //         <h3>Title</h3>
+  //         <div>{bookDetails.volumeInfo.title}</div>
+  //         <div class="col-md-12 bg-light text-right">
+  //           <button
+  //             type="button"
+  //             class="btn btn-success"
+  //             onClick={() => {
+  //               window.open(
+  //                 `https://vpl.bibliocommons.com/v2/search?query=${bookDetails.volumeInfo.title}`
+  //               );
+  //             }}
+  //           >
+  //             Borrow book
+  //           </button>
+  //         </div>
+  //         <h3>Subtitle</h3>
+  //         <div>{bookDetails.volumeInfo.subtitle}</div>
+  //         <h3>Description</h3>
+  //         <div>{bookDetails.volumeInfo.description}</div>
+  //         <h3>Reviews</h3>
+  // {
+  //   !editReview && (
+  //     <button
+  //       onClick={() => {
+  //         setAddReview(true);
+  //         setEditReview(true);
+  //       }}
+  //     >
+  //       Add New Review{" "}
+  //     </button>
+  //   );
+  // }
+  // {
+  //   !editReview ? (
+  //     reviewsDetails.map((review, index) => {
+  //       return (
+  //         <div key={index}>
+  //           {review.review_text}
+  //           <button onClick={() => deleteReview(review._id)}>Delete</button>
+  //           <button
+  //             onClick={() => {
+  //               setEditReview(true);
+  //               setEditIndex(index);
+  //             }}
+  //           >
+  //             Update
+  //           </button>
+  //         </div>
+  //       );
+  //     })
+  //   ) : (
+  //     <div>
+  //       <input
+  //         placeholder="Enter New Review"
+  //         value={editReviewText}
+  //         onChange={(e) => setEditReviewText(e.target.value)}
+  //       />
+  //       {addReview ? (
+  //         <button
+  //           onClick={() => {
+  //             createReview(editReviewText);
+  //           }}
+  //         >
+  //           Create{" "}
+  //         </button>
+  //       ) : (
+  //         <button
+  //           onClick={() =>
+  //             saveReview(reviewsDetails[editIndex]._id, editReviewText)
+  //           }
+  //         >
+  //           Save
+  //         </button>
+  //       )}
+  //     </div>
+  //   );
+  // }
+  //         {viewAllReviews.map((review, index) => {
+  //           return <div key={index}>{review.review_text}</div>;
+  //         })}
+  //       </>
+  //     )}
+  //   </>
+  // );
 }
